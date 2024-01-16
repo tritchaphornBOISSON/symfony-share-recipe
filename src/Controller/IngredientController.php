55815c1f7dc652ff0345af217e48b2a1684d266e
Tritchaphorn BOISSON <tritchaphorn@gmail.com>
2024-01-16 11:39:02 +0100

    Implement pagination

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -2,19 +2,32 @@
 
 namespace App\Controller;
 
-use App\Entity\Ingredient;
 use App\Repository\IngredientRepository;
-use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IngredientController extends AbstractController
 {
-    #[Route('/ingredient', name: 'ingredient')]
-    public function index(IngredientRepository $ingredientRepository): Response
+    /**
+     * Fetch and display all the ingredients.
+     *
+     * @param IngredientRepository $ingredientRepository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+    #[Route('/ingredient', name: 'ingredient', methods: ['GET'])]
+    public function index(IngredientRepository $ingredientRepository, PaginatorInterface $paginator, Request $request): Response
     {
-        $ingredients = $ingredientRepository->findAll();
+        $query = $ingredientRepository->findAll();
+        $ingredients = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            12
+        );
 
         return $this->render('pages/ingredient/index.html.twig', [
             'ingredients' => $ingredients,
